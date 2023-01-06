5ab6cb38c2cf85a4c54fd3662d2ba25145231977
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2023-01-06 18:29:15 +0530

    landing page

--- src/common/UI/home/Sait.jsx
+++ src/common/UI/home/Sait.jsx
@@ -6,7 +6,7 @@ const Sait = (props) => {
       <div className="text-center w-20">
         <h1 className="text-6xl md:text-8xl font-bold">{props.letter}</h1>
       </div>
-      <p className="self-end text-xs md:text-xl mb-2 tracking-[8px] md:tracking-[10px] md:mx-2 font-semibold">
+      <p className="self-end text-sm md:text-xl mb-2 tracking-[7px] md:tracking-[10px] md:mx-2 font-semibold">
         {props.full}
       </p>
     </div>
