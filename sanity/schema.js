ae6d649c4b8adef4de85f5f709d41a454882e3f8
ameennm <ameennm014@gmail.com>
2023-08-15 12:30:34 +0530

    eecutive schema

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,7 +2,8 @@ import blockContent from './schemas/blockContent'
 import category from './schemas/category'
 import event from './schemas/event'
 import author from './schemas/author'
+import executive from './schemas/executive'
 
 export const schema = {
-  types: [event, author, category, blockContent],
+  types: [event, author, category, blockContent,executive],
 }
