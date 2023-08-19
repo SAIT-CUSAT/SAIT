59cf96a6da0163fb94a05564fe7f8a1f249f5377
Pavan-1802 <119658149+Pavan-1802@users.noreply.github.com>
2023-08-19 17:24:46 +0530

    executives mapped

--- src/common/UI/home/Executives.jsx
+++ src/common/UI/home/Executives.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ExecutiveCards from "./ExecutiveCards";
+import { client } from '../../../../sanity/lib/client'
 
 function Executives() {
+  const [executives, setExecutives] = useState([]);
+  useEffect(() => {
+    const query = `*[_type == "executive"] {
+      name,
+      position,
+      "imageUrl": image.asset->url,
+      _createdAt
+    }`;
+    client.fetch(query)
+      .then(data => {
+        console.log("Fetched data:", data);
+        const sortedExecutives = data.sort((a, b) => new Date(a._createdAt) - new Date(b._createdAt));
+        setExecutives(sortedExecutives);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
-    <div className=" md:my-0 my-5">
+    <div className="md:my-0 my-5">
       <h1 className="md:text-3xl md:my-0 my-5 text-2xl font-bold md:tracking-[1.5em] text-center font-sans uppercase md:mb-10">
         EXECUTIVE MEMBERS
       </h1>
       <div className="flex flex-wrap justify-center items-center gap-5">
-        <ExecutiveCards />
-        <ExecutiveCards />
-        <ExecutiveCards />
-        <ExecutiveCards />
+        {executives.map(executive => (
+          <div key={executive.name}>
+            <ExecutiveCards
+              name={executive.name}
+              position={executive.position}
+              image={executive.imageUrl}
+            />
+          </div>
+        ))}
       </div>
       <p className="text-right p-8 font-bold text-blue-900">VIEW ALL -&gt;</p>
     </div>
