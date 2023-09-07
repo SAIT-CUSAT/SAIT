0563a7aed9bce12eb7fea61da1a71fe3225bbc08
DareDevilStudios <studios.daredevil@gmail.com>
2023-09-07 18:42:26 +0530

    event details model is initaiated

---
+++ src/common/components/Event_data.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Event_data = () => {
+  return (
+    <div>
+      <section class="bg-white ">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+          <div class="font-light text-gray-500 sm:text-lg ">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">
+              We didn't reinvent the wheel
+            </h2>
+            <p class="mb-4">
+              We are strategists, designers and developers. Innovators and
+              problem solvers. Small enough to be simple and quick, but big
+              enough to deliver the scope you want at the pace you need. Small
+              enough to be simple and quick, but big enough to deliver the scope
+              you want at the pace you need.
+            </p>
+            <p>
+              We are strategists, designers and developers. Innovators and
+              problem solvers. Small enough to be simple and quick.
+            </p>
+          </div>
+          <div class="grid grid-cols-2 gap-4 mt-8">
+            <img
+              class="w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+              alt="office content 1"
+            />
+            <img
+              class="mt-4 w-full lg:mt-10 rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+              alt="office content 2"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Event_data;
