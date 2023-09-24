7858fdf724bcaafa2bcd12525e817bfc70d5438a
DareDevilStudios <studios.daredevil@gmail.com>
2023-09-24 23:18:53 +0530

    &apos; implemented

--- src/common/components/Event_data.jsx
+++ src/common/components/Event_data.jsx
@@ -7,7 +7,7 @@ const Event_data = () => {
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           <div class="font-light text-gray-500 sm:text-lg ">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">
-              We didn't reinvent the wheel
+              We didn&apos;t reinvent the wheel
             </h2>
             <p class="mb-4">
               We are strategists, designers and developers. Innovators and
